45df43f509fb8a80087ff7b83505a0afc9909b4d
Brad Foster <btf5048@gmail.com>
2015-06-04 11:33:49 -0400

    map css

--- application/server.js
+++ application/server.js
@@ -33,10 +33,6 @@ var app = express();
 app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.json());
 
-app.get('/', function(req, res){
-  res.sendfile('/public/ageVerification.html');
-});
-
 app.all('/*', function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "X-Requested-With, Content-Type");
